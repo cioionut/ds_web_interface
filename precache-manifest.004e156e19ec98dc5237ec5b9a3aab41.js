67c1677899f842c17413b279d48d412575b2d41c
Ionut Ciocoiu <ionutnciocoiu@gmail.com>
2019-04-02 22:16:21 +0300

    Updates

---
+++ precache-manifest.004e156e19ec98dc5237ec5b9a3aab41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3dbd6f57de59aab654bb",
+    "url": "/nlu-demo/static/css/main.295c21d3.chunk.css"
+  },
+  {
+    "revision": "3dbd6f57de59aab654bb",
+    "url": "/nlu-demo/static/js/main.3dbd6f57.chunk.js"
+  },
+  {
+    "revision": "34a32f05befd523db640",
+    "url": "/nlu-demo/static/css/1.a92a15f5.chunk.css"
+  },
+  {
+    "revision": "34a32f05befd523db640",
+    "url": "/nlu-demo/static/js/1.34a32f05.chunk.js"
+  },
+  {
+    "revision": "0060c1578a3190587ddf",
+    "url": "/nlu-demo/static/js/runtime~main.0060c157.js"
+  },
+  {
+    "revision": "5bcb86f571017e2ea6ce1ef834b38fe5",
+    "url": "/nlu-demo/index.html"
+  }
+];
