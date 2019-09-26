629bd989e73d401cb96b379bf97e1361817d7b1d
Ionut Ciocoiu <ionut.ciocoiu@footprintsforretail.com>
2019-09-26 17:06:57 +0300

    Add chatbox

--- src/components/dialog/dialog.js
+++ src/components/dialog/dialog.js
@@ -1,24 +1,75 @@
 import React, { Component } from 'react';
+import './dialog.css';
 
-import NLUResponse from '../nluresponse/nluresponse'
 
-import {
-  Container,
-  Row,
-  Col,
-  Form, FormGroup, Input, Button
-} from 'reactstrap';
+function Message(props) {
+
+  if (props.isMe === true)
+    return (
+      <div className="row justify-content-end">
+        <div className="card message-card bg-lightblue m-1">
+            <div className="card-body p-2">
+                <span className="mx-2">{props.value}</span>
+            </div>
+        </div>
+      </div>
+    );
+  return (
+    <div className="row">
+      <div className="card message-card m-1">
+          <div className="card-body p-2">
+              <span className="mx-2">{props.value}</span>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+class MessageList extends Component {
+  constructor(props) {
+    super(props);
+    this.scrollToBottom = this.scrollToBottom.bind(this);
+  }
+  scrollToBottom = () => {
+    this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+  }
+  
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+  
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
+  render() {
+    const objMessages = this.props.messages
+    let messageList = objMessages.map((message, idx) => 
+      <Message value={message.text} isMe={message.isMe} key={idx}/>
+    );
+    return (
+      <div className="container-fluid message-scroll">
+        {messageList}
+        <div style={{ float:"left", clear: "both" }}
+            ref={(el) => { this.messagesEnd = el; }}>
+        </div>
+      </div>
+    )
+  }
+}
+
 
 class Dialog extends Component {
   constructor(props) {
     super(props);
-    this.startUtt = "please show me flights from pittsburgh to new york city on wednesday morning serving breakfast";
+    this.startUtt = "...";
     this.urlStr = 'https://cionlu.herokuapp.com/api/v1/nlu';  // 'http://localhost:5000/api/v1/nlu';
 
     this.handleUtteranceChange = this.handleUtteranceChange.bind(this);
     this.handleSubmitFrom = this.handleSubmitFrom.bind(this);
 
     this.state = {
+      dialog: [],
       nluResponse: {
         "entities": [
           {
@@ -34,7 +85,8 @@ class Dialog extends Component {
             "intent_type": "t1"
         },
         text: ""
-      }
+      },
+      utterance: ""
     };
   }
 
@@ -52,6 +104,12 @@ class Dialog extends Component {
 
     let url = new URL(this.urlStr);
     let params = {utterance: this.state.utterance};
+    let dialog = this.state.dialog;
+    dialog.push({
+      isMe: true,
+      text: this.state.utterance
+    });
+
     url.search = new URLSearchParams(params);
 
     fetch(url, {
@@ -65,30 +123,15 @@ class Dialog extends Component {
     })
     .then(response => {      
       if (response)
-        this.setState({nluResponse: response})
-    })
-    .catch(error => console.error('Error:', error));
-  }
+        dialog.push({
+          isMe: false,
+          text: "bot: " + this.state.utterance
+        });
+        this.setState({
+          nluResponse: response,
+          dialog: dialog
+        });
 
-  componentDidMount() {
-    
-    this.setState({utterance: this.startUtt});
-
-    let url = new URL(this.urlStr);
-    let params = {utterance: this.startUtt};
-    url.search = new URLSearchParams(params);
-
-    fetch(url, {
-      method: 'GET'
-    })
-    .then(res => {
-      if (res.status !== 200)
-        return undefined
-      return res.json();
-    })
-    .then(response => {
-      if (response)
-        this.setState({nluResponse: response})
     })
     .catch(error => console.error('Error:', error));
   }
@@ -96,32 +139,46 @@ class Dialog extends Component {
   render() {
     return (
       <div>
-      <Container className="nlu-content">
-        <Row>
-          <Col>
-            <h5>HH</h5>
-            {/* <NLUResponse
-              intent={this.state.nluResponse.intent}
-              entities={this.state.nluResponse.entities}
-            /> */}
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h5>State something</h5>
-            <Form onSubmit={this.handleSubmitFrom}>
-              <FormGroup>
-                <Input type="textarea" name="utterance" id="utterance"
-                  value={this.state.utterance}
-                  onChange={this.handleUtteranceChange} 
-                  placeholder={this.startUtt} 
-                  />
-                <Button className="float-right submit-btn" color="primary" type="submit">Analyze</Button>
-              </FormGroup>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
+        <div className="container-fluid chat-container">
+          <div className="row h-100">
+            <div className="col d-flex p-0">
+                <div className="card">
+                    <div className="card-header bg-darkblue text-white py-1 px-2">
+                        <div className="d-flex flex-row justify-content-start">
+                            <div className="col-1 p-1">
+                                <button className="btn text-white bg-darkblue p-0 hover-color-lightgray">
+                                    <i className="fas fa-bars fa-2x"></i>
+                                </button>
+                            </div>
+                            <div className="col">
+                                <div className="my-0">
+                                    <b>Active d</b>
+                                </div>
+                                <div className="my-0">
+                                    <small>stats 0000</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-body bg-lightgrey d-flex flex-column p-0">
+                        <MessageList messages={this.state.dialog} />
+                        <form onSubmit={this.handleSubmitFrom}>
+                          <div className="form-group">
+                            <input type="text" className="form-control" id="utterance" aria-describedby="utteranceHelp"
+                              value={this.state.utterance}
+                              onChange={this.handleUtteranceChange} 
+                              placeholder={this.startUtt}
+                            />
+                          </div>
+                          <button type="submit" className="btn btn-primary float-right">Send
+                          {/* <i className="fab fa-telegram-plane fa-2x"></i> */}                    
+                          </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
